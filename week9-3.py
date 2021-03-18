5e5eab46cdd6044205d985d5f4ff7754bfbc0f96
hopeful11 <77275763+hopeful11@users.noreply.github.com>
2021-03-18 16:26:40 +0200

    week9

---
+++ week9-3.py
@@ -0,0 +1,16 @@
+from collections import deque
+def generate(n):
+    q = deque()
+    q.append('1')
+    for i in range(n):
+        front = str(q.popleft())
+
+        q.append(front + '0')
+        q.append(front + '1')
+
+        print(front, end=' ')
+
+
+if __name__ == '__main__':
+    n = 10
+    generate(n)
